5aff78fc384e0f8d03225082957e17e55db9b7ab
Natnael Biruk <natnaelbiruk3@gmail.com>
2023-07-15 09:57:19 +0300

    card element and item list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
+import ItemList from "./components/ItemList";
 
 function App() {
-
-  return <></>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 p-3 pb-8">
+      <ItemList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,25 @@
+import Card from "./Card";
+
+function ItemList() {
+  return (
+    <div className="h-full grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  m-3">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default ItemList;
